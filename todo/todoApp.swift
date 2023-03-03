ba111f8cafd0e7db45fefbdee590518bb3fa25f1
lewiscasewell <email>
2023-03-03 22:34:43 +0000

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Lewis Casewell on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
